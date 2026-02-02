2fe228636261fc8f9c777900204869d152e3cd77
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-02-02 14:03:43 +0200

    apis

--- src/components/Nature.jsx
+++ src/components/Nature.jsx
@@ -1,4 +1,6 @@
 const Nature = () => {
-  return <div>Nature Component</div>;
+  return <div>Nature Component
+    <p>nature api: `https://pixabay.com/api/?q=$nature&page=$1&key=50843029-710c41440238bfac4a870a64c&image_type=photo&orientation=horizontal&per_page=5</p>
+  </div>;
 };
 export default Nature;
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,6 @@
 const News = () => {
-  return <div>News Component</div>;
+  return <div>News Component
+    <p>pets api: https://newsapi.org/v2/everything?q=pets&from=2026-01-02&sortBy=publishedAt&apiKey=c84034d791c44ae78c99f5457ea64f3f</p>
+  </div>;
 };
 export default News;
