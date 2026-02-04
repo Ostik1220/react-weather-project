897b65882162f2f5ed7fa686c38ba02514b1506e
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-02-05 00:01:44 +0200

    save modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,26 +6,25 @@ const Modal = () => {
   return <div className={style.backdrop}>
     <div className={style.modal}>
       <h2 className={style.title}>Sign up</h2>
-    <form action="">
-      <ul>
-        <li>
+    <form action="" className={style.form}>
+      <ul className={style.list}>
+        <li className={style.item}>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" required className={style.input}/>
         </li>
-        <li>
+        <li className={style.item}>
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" required className={style.input}/>
         </li>
-        <li>
+        <li className={style.item}>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
-        </li>
-        <li>
-        <Button variant="contained" color="default">
-          Sign Up
-        </Button>
+          <input type="password" id="password" name="password" required className={style.input}/>
         </li>
       </ul>
+              <Button variant="contained" color="default">
+          Sign Up
+        </Button>
+        <p className={style.text}>Already have an account? <a href="./" className={style.link}>Log In</a></p>
     </form>
     </div>
   </div>;
